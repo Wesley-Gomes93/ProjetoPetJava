f0b3713e30e25452b21c1f6233da098171941bb9
Wesley-Gomes93 <wesleyluizsilvagomes@gmail.com>
2024-02-27 23:15:53 -0300

    Atualizar, consultar

--- src/test/java/testaraujo/Pet.java
+++ src/test/java/testaraujo/Pet.java
@@ -7,8 +7,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItem;
 
-public class Pet {
+public class Pet{
     String uri = "https://petstore.swagger.io/v2/pet";
 
     public String lerJson(String caminhoJson) throws IOException {
@@ -16,7 +19,7 @@ public class Pet {
 
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
-    @Test
+    @Test(priority = 1 )
     public void criarPet() throws IOException {
         String jsonBody = lerJson("src/data/pet1.json");
 
@@ -24,16 +27,66 @@ public class Pet {
                 .contentType("application/json")
                 .log().all()
                 .body(jsonBody)
-        .when()
+                .when()
                 .post(uri)
-        .then()
+                .then()
                 .log().all()
-        .statusCode(200);
-
-
-
-
-
+                .statusCode(200)
+                .body("name", is("Thor"))
+                .body("status", is("available"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("araujo"))
+        ;
 
     }
+
+    @Test(priority = 2 )
+    public void consultarPetPorId(){
+        String petId = "06111993";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .when()
+                .get(uri + "/" + petId)
+                .then()
+                .log().all()
+                .statusCode(200)
+
+        ;
+    }
+    @Test
+    public void atualizarPet() throws IOException {
+        String jsonBody = lerJson("src/data/petAtualizado.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Wesley Teste Araujo"))
+                .body("status", is("sold"));
+    }
+
+
+    @Test(priority=0)
+    public void consultarStatus(){
+        String uri = "https://petstore.swagger.io/v2/pet";
+        String status = "pending";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .when()
+                .get(uri + "/findByStatus?status=" + status)
+                .then()
+                .log().all()
+                .statusCode(200);
+
+    }
+
 }
